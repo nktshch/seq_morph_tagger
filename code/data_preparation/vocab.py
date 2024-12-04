fixed initialization, oaxe calculation, changed predict.py

code/data_preparation/vocab.py
@@ -115,6 +115,7 @@ class Vocab:
                         wordforms.add(self.conf['NUM'])
                     else:
                         wordforms.add(token.form.lower())
+                    # wordforms.add(token.form)
         wordforms.add(self.conf['NUM'])
         wordforms = [self.conf['PAD'], self.conf['UNK']] + sorted(list(wordforms))
         return get_dictionaries(wordforms)
@@ -135,7 +136,7 @@ class Vocab:
         Returns:
             tuple: Dictionaries with grammeme->index and index->grammeme pairs.
         """
-        
+
         grammemes = set()
         from collections import defaultdict
         frequencies = defaultdict(int)
@@ -201,7 +202,7 @@ class Vocab:
         singletons = [token for token, cnt in counter.items() if cnt == 1]
         return get_dictionaries(singletons)
 
-    def sentence_to_indices(self, sentence, sentence_pyconll=None,
+    def sentence_to_indices(self, sentence, sentence_pyconll=None, training=True,
                             unk_word_id=1, unk_char_id=1, unk_grammeme_id=3):
         """Returns indices of words, chars, and grammemes for a sentence. Used by CustomDataset and in predict.
 
@@ -216,6 +217,7 @@ class Vocab:
             sentence_pyconll (pyconll.unit.conll.Conll, default None): Sentence in pyconll format. Used for grammemes.
                 During inference, there will be no pyconll sentences, which means that method should return list
                 of n_words empty lists as labels.
+            training (bool, default True): If True, singletons will randomly be substituted by unk token.
             unk_word_id (int, default 1): The id of the unk word token in dictionary.
             unk_char_id (int, default 1): The id of the unk char token in dictionary.
             unk_grammeme_id (int, default 3): The id of the unk grammeme token in dictionary.
@@ -226,7 +228,7 @@ class Vocab:
         for word in sentence:
             if word.isdigit():
                 word_ids = [self.vocab["word-index"][self.conf['NUM']]]
-            elif word.lower() in self.vocab["singleton-index"].keys() and np.random.rand() < self.conf["singleton_substitution"]:
+            elif training is True and word.lower() in self.vocab["singleton-index"].keys() and np.random.rand() < self.conf["singleton_substitution"]:
                 word_ids = [self.vocab["word-index"][self.conf['UNK']]]
             else:
                 word_ids = [self.vocab["word-index"].get(word.lower(), unk_word_id)]

code/predict.py
@@ -12,6 +12,7 @@ import torch
 from torch import cuda
 import pickle
 import pathlib
+from pathlib import Path
 import fasttext
 import pyconll
 from tqdm import tqdm
@@ -31,74 +32,6 @@ def load_model_vocab(model_file, vocab_file):
     model.eval()
 
     return conf, vocab, model
-
-# TODO: rewrite or remove
-# def predict(model_file, vocab_file, sentence, sentence_pyconll=None):
-#     """Uses saved model to assign tags to words for list of sentences and save them in a file.
-#
-#     Args:
-#         model_file (string): File containing saved model parameters, .pt.
-#         vocab_file (string): File containing vocab dictionary.
-#         sentence (list): List of words in a sentence.
-#         sentence_pyconll (list): If there is a sentence in pyconll format, it can be used to calculate accuracy.
-#     """
-#
-#     conf, state_dict = torch.load(model_file)
-#     with open(vocab_file, 'rb') as vf:
-#         vocab = pickle.load(vf)
-#
-#     model = Model(conf, vocab)
-#     model.load_state_dict(state_dict)
-#     model.to(device)
-#     model.eval()
-#
-#     words, labels = vocab.sentence_to_indices(sentence, sentence_pyconll)
-#     words, chars, labels, _ = collate_batch([((words, labels), sentence)])
-#
-#     assert _[0] == sentence, f"{_[0]} is not equal to {sentence}"
-#
-#     words = words.to(device)
-#     chars = chars.to(device)
-#     if labels is not None:
-#         labels = labels.to(device)
-#
-#     unk_id = vocab.vocab["word-index"][conf["UNK"]]
-#
-#     # print(words)
-#     oov = None
-#     mask_embeddings = torch.zeros(words.shape, dtype=bool, device=device)
-#     if unk_id in words:
-#         print("Some words are not in vocab!")
-#         ft = fasttext.load_model("./Russian/cc.ru.300.bin")
-#         fasttext_embeddings = []
-#         for i, word_index in enumerate(words):
-#             if word_index.item() == unk_id:
-#                 print(f"'{sentence[i].lower()}' is not in vocab")
-#                 if sentence[i].lower() in ft.words:
-#                     print(f"Found '{sentence[i].lower()}' in fastText")
-#                     fasttext_embeddings += [ft[sentence[i].lower()]]
-#                     mask_embeddings[i] = True
-#
-#         fasttext_embeddings = torch.tensor(np.array(fasttext_embeddings)).to(device)
-#         assert mask_embeddings.sum().item() == fasttext_embeddings.shape[0]
-#         oov = (fasttext_embeddings, mask_embeddings)
-#
-#
-#     with torch.no_grad():
-#         predictions, probabilities = model(words, chars, None, oov=oov)
-#     grammemes = predictions_to_grammemes(vocab.vocab, predictions.permute(1, 0))
-#
-#     for word, tag in zip(sentence, grammemes):
-#         print(word)
-#         print("PREDICTED")
-#         print(tag)
-#         print("\n")
-#
-#     # check accuracy if labels are available
-#     if labels is not None:
-#         targets = labels[1:]
-#         correct, total = calculate_accuracy(vocab, conf, predictions, targets, mask_embeddings)
-#         print(f"Correct: {correct}, accuracy: {correct / total}")
 
 
 def predictions_to_grammemes(vocabulary, predictions):
@@ -121,13 +54,12 @@ def predictions_to_grammemes(vocabulary, predictions):
     return tags
 
 
-def fill_conllu(conf, vocab, model, conll_input, conll_output):
+def fill_conllu(conf, vocab, model, uniq_words, conll_input, conll_output):
     conll = pyconll.load_from_file(conll_input)
 
-    uniq_words = get_uniq_words(conf, vocab, conll)
     print(f"{len(uniq_words)} oov words")
     print("Loading fastText...")
-    ft = fasttext.load_model("./Russian/cc.ru.300.bin")
+    ft = fasttext.load_model("./filled_conllu/cc.ru.300.bin")
     oov_pretrained_vocab = {}
     for word in ft.words:
         if word.lower() in uniq_words:
@@ -135,13 +67,13 @@ def fill_conllu(conf, vocab, model, conll_input, conll_output):
 
     print("Filling file...")
     progress_bar = tqdm(conll, total=len(conll), colour='#bbbbbb')
-    for sentence in progress_bar:
+    for number, sentence in enumerate(progress_bar):
         word_list = []
         for token in sentence:
             if '.' not in token.id and '-' not in token.id:
                 word_list += [token.form]
 
-        words, labels = vocab.sentence_to_indices(word_list, None)
+        words, labels = vocab.sentence_to_indices(word_list, None, training=False)
         words, chars, _, __ = collate_batch([((words, labels), word_list)])
         words = words.to(device)
         chars = chars.to(device)
@@ -166,9 +98,11 @@ def fill_conllu(conf, vocab, model, conll_input, conll_output):
         grammemes = predictions_to_grammemes(vocab.vocab, predictions.permute(1, 0))
 
         for label, token in zip(grammemes, sentence):
-            token.upos = label[0][4:]
+            other_grammemes = [g for g in label if "POS" not in g]
+            pos_grammeme = next(iter([g for g in label if "POS" in g]), "POS=Did_not_predict")
+            token.upos = pos_grammeme[4:]
             feats = defaultdict(list)
-            for grammeme in label[1:]:
+            for grammeme in other_grammemes:
                 if grammeme == conf["EOS"]:
                     break
                 key, feat = grammeme.split("=")
@@ -177,27 +111,6 @@ def fill_conllu(conf, vocab, model, conll_input, conll_output):
 
     with open(conll_output, 'w+', encoding='utf-8') as f:
         conll.write(f)
-
-
-def conllu_accuracy(conll_true, conll_predicted):
-    conll_true = pyconll.load_from_file(conll_true)
-    conll_predicted = pyconll.load_from_file(conll_predicted)
-    correct, total = 0, 0
-    print("Calculating accuracy...")
-    for sentence_true, sentence_predicted in zip(conll_true, conll_predicted):
-        for word_true, word_predicted in zip(sentence_true, sentence_predicted):
-            if '.' not in word_true.id and '-' not in word_true.id:
-                total += 1
-                if word_true.upos != word_predicted.upos:
-                    break
-                tag_true = []
-                tag_predicted = []
-                tag_true += [key + "=" + feat for key in list(word_true.feats) for feat in list(word_true.feats[key])]
-                tag_predicted += [key + "=" + feat for key in list(word_predicted.feats) for feat in list(word_predicted.feats[key])]
-                equal = set(tag_true) == set(tag_predicted)
-                correct += int(equal)
-    print(f"Accuracy: {correct / total}")
-    return correct / total
 
 
 def get_uniq_words(conf, vocab, conll):
@@ -219,33 +132,71 @@ def get_uniq_words(conf, vocab, conll):
 
 
 def construct_df(conll_true, conll_predicted, uniq_words):
+    print(len(uniq_words))
     conll_true = pyconll.load_from_file(conll_true)
     conll_predicted = pyconll.load_from_file(conll_predicted)
     data = []
     for sentence_true, sentence_predicted in zip(conll_true, conll_predicted):
+        neighbors_uniq = False
+        for word_true in sentence_true:
+            if '.' not in word_true.id and '-' not in word_true.id and word_true.form.lower() in uniq_words:
+                neighbors_uniq = True
+                break
         for word_true, word_predicted in zip(sentence_true, sentence_predicted):
             if '.' not in word_true.id and '-' not in word_true.id:
+                label_true, label_predicted = [], []
                 uniq = True if word_true.form.lower() in uniq_words else False
-                data += [(word_true.form, "POS=" + word_true.upos, "POS=" + word_predicted.upos, uniq, word_true.upos != word_predicted.upos)]
+
+                label_true += ["POS=" + word_true.upos]
                 for key_true in list(word_true.feats):
-                    # zip_longest should be used instead
-                    for feat_true, feat_predicted in zip(word_true.feats[key_true], word_predicted.feats.get(key_true, [])):
-                        if feat_predicted is None:
-                            data += [(word_true.form, key_true + "=" + feat_true, '', uniq, True)]
-                        else:
-                            data += [(word_true.form, key_true + "=" + feat_true, key_true + "=" + feat_predicted, uniq, feat_true != feat_predicted)]
+                    for feat_true in word_true.feats[key_true]:
+                        label_true += [key_true + "=" + feat_true]
+                tt = '|'.join(label_true)
 
+                label_predicted += ["POS=" + word_predicted.upos]
+                for key_predicted in list(word_predicted.feats):
+                    for feat_predicted in word_predicted.feats[key_predicted]:
+                        label_predicted += [key_predicted + "=" + feat_predicted]
+                pt = '|'.join(label_predicted)
 
-    df = pd.DataFrame(data=data, columns=['word', 'tt_full', 'pt_full', 'uniq', 'err'])
+                data += [(word_true.form, tt, pt, neighbors_uniq, uniq, tt != pt)]
+
+    df = pd.DataFrame(data=data, columns=['word', 'tt_full', 'pt_full', 'neighbors_uniq', 'uniq', 'err'])
     df['tt_pos'] = df.tt_full.str.extract(r'POS=([A-Z]+)', expand=False).fillna('')
     df['pt_pos'] = df.pt_full.str.extract(r'POS=([A-Z]+)', expand=False).fillna('')
 
     # set morph predictions
-    df['tt_morph'] = df.tt_full.str.replace(r'(\|POS=[A-Za-z]+\|)', '|').str.replace(r'(\|?POS=[A-Za-z]+\|?)', '')
-    df['pt_morph'] = df.pt_full.str.replace(r'(\|POS=[A-Za-z]+\|)', '|').str.replace(r'(\|?POS=[A-Za-z]+\|?)', '')
+    df['tt_morph'] = df.tt_full.str.replace(r'(POS=[A-Za-z]+\|?)', '', regex=True)
+    df['pt_morph'] = df.pt_full.str.replace(r'(POS=[A-Za-z]+\|?)', '', regex=True)
 
     return df
 
+
+def analyze_df(df):
+    correct_df = df[df.err == False]
+    error_df = df[df.err == True]
+    print(f"errors: {len(error_df)}")
+    uniq_df = df[df.uniq == True]
+    vocab_df = df[df.uniq == False]
+    neighbors_uniq_df = df[df.neighbors_uniq == True]
+    neighbors_vocab_df = df[df.neighbors_uniq == False]
+
+    error_uniq_df = error_df[error_df.uniq == True]
+    error_vocab_df = error_df[error_df.uniq == False]
+    error_neighbors_uniq_df = error_df[error_df.neighbors_uniq == True]
+    error_neighbors_vocab_df = error_df[error_df.neighbors_uniq == False]
+    print(f"erroneous words that neighbor uniq: {len(error_neighbors_uniq_df)}, {len(error_neighbors_uniq_df) / len(neighbors_uniq_df)}")
+    print(f"erroneous words that neighbor vocab: {len(error_neighbors_vocab_df)}, {len(error_neighbors_vocab_df) / len(neighbors_vocab_df)}")
+    print(f"erroneous words that are uniq: {len(error_uniq_df)}, {len(error_uniq_df) / len(uniq_df)}")
+    print(f"erroneous words that are vocab: {len(error_vocab_df)}, {len(error_vocab_df) / len(vocab_df)}")
+    # category_df = {}
+    # print(len(df))
+    # for cat in ['Case', 'Gender', 'Number', 'VerbForm', 'Tense', 'Person', 'Degree', 'Aspect', 'Voice', 'Mood']:
+    #     category_df[cat] = df.loc[df['tt_morph'].str.contains(cat)]
+    #     category_df['error_' + cat] = category_df[cat][category_df[cat].err == True]
+    #     print(f"errors for {cat} - {len(category_df['error_' + cat])}, "
+    #           f"{len(category_df['error_' + cat]) / len(category_df[cat]):.3f}")
+    # print(error_vocab_df)
 
 def calculate_accuracy_df(df):
     from sklearn.metrics import accuracy_score
@@ -267,21 +218,54 @@ def calculate_accuracy_df(df):
     )
 
 
+def evaluate_models():
+    trained_models_folder = "./trained_models"
+
+    p = Path(trained_models_folder)
+
+    # temp = pathlib.PosixPath
+    # pathlib.PosixPath = pathlib.WindowsPath
+    for seed_number in range(5):
+        model_paths = [str(x) for x in p.glob(f"*direct_order*/Russian-SynTagRus/seed_{seed_number}/model.pt")]
+        vocab_paths = [str(x) for x in p.glob("*/Russian-SynTagRus/vocab.pickle")]
+
+        for model_path, vocab_path in zip(model_paths, vocab_paths):
+            model_name = model_path.split('\\')[1]
+            print(f"{model_name} run {seed_number}")
+            conf, vocab, model = load_model_vocab(model_path, vocab_path)
+            # pathlib.PosixPath = temp
+            uniq_words = get_uniq_words(conf, vocab, pyconll.load_from_file("./data/UD_Russian-SynTagRus/ru_syntagrus-ud-dev.conllu"))
+            fill_conllu(conf, vocab, model, uniq_words, "./filled_conllu/Russian-SynTagRus/ru_syntagrus-ud-dev.conllu",
+                        f"./filled_conllu/Russian-SynTagRus/ru_syntagrus-ud-dev_{model_name}_{seed_number}.conllu")
+            df = construct_df("./filled_conllu/Russian-SynTagRus/ru_syntagrus-ud-dev.conllu",
+                              f"./filled_conllu/Russian-SynTagRus/ru_syntagrus-ud-dev_{model_name}_{seed_number}.conllu", uniq_words)
+
+            analyze_df(df)
+
+            with open(f"./filled_conllu/results_dev_syntagrus_{seed_number}.txt", "w+") as file:
+                file.write(f"\n{model_name}\n")
+                results = calculate_accuracy_df(df)
+                for key in results:
+                    file.write(f"{key}: {results[key]:.5}\n")
+
+    # model_path = "./trained_models/direct_order/Russian/seed_0/model.pt"
+    # vocab_path = "./trained_models/direct_order/Russian/vocab.pickle"
+    # conf, vocab, model = load_model_vocab(model_path, vocab_path)
+    #
+    # model.to(device)
+    # model.eval()
+    #
+    # uniq_words = get_uniq_words(conf, vocab, pyconll.load_from_file("./data/UD_Russian/ru-ud-dev.conllu"))
+    # fill_conllu(conf, vocab, model, uniq_words, "./filled_conllu/Russian/ru-ud-dev.conllu",
+    #             f"./filled_conllu/Russian/ru-ud-dev_direct_order_0b.conllu")
+    # df = construct_df("./filled_conllu/Russian/ru-ud-dev.conllu",
+    #                   f"./filled_conllu/Russian/ru-ud-dev_direct_order_0b.conllu", uniq_words)
+    # results = calculate_accuracy_df(df)
+    # for key in results:
+    #     print(f"{key}: {results[key]:.5}")
+
+
 if __name__ == "__main__":
-    # sentence_ = "Безгачиха -- деревня в Бабушкинском районе Вологодской области .".split()
-    # sentence_pyconll = pyconll.load.load_from_file("./Russian/test.conllu")[5]
-    # predict("./Russian/seed_0/model.pt", "./Russian/vocab.pickle", sentence_, sentence_pyconll)
-
-
-
-    # conllu_accuracy("./Russian/train.conllu", "./Russian/train_filled.conllu")
-
-    for i in range(5):
-        conf_, vocab_, model_ = load_model_vocab(f"./Russian/seed_{i}/model.pt", "./Russian/vocab.pickle")
-        uniq_words_ = get_uniq_words(conf_, vocab_, pyconll.load_from_file("./Russian/test.conllu"))
-        fill_conllu(conf_, vocab_, model_,
-                    "./Russian/test.conllu", f"./Russian/test_{i}_filled.conllu")
-        df_ = construct_df("./Russian/test.conllu", f"./Russian/test_{i}_filled.conllu", uniq_words_)
-        print(calculate_accuracy_df(df_))
-
-
+    evaluate_models()
+# TODO: check that ABSOLUTELY everything is as in author's code and paper. Mark questionable places. Possible:
+# pyconll loading data, metrics, padding learning, scale factor, loss function, default parameters in torch and tf

code/trainer.py
@@ -205,7 +205,7 @@ class Trainer(nn.Module):
         best_permutations_ = torch.tensor(range(probabilities_.shape[2])).to(targets).repeat((probabilities_.shape[0], 1))
         for i in range(targets.shape[0]):  # for 1 sequence in a batch
             target_row = targets[i]
-            n_nonpad = target_row.ne(0).sum()  # to exclude padding (0 is pad_id)
+            n_nonpad = target_row.ne(0).sum() - 1 # to exclude padding and eos token (0 is pad_id)
             if n_nonpad == 0:  # skip if row is padding itself
                 continue
             target_row = target_row[:n_nonpad]  # remove padding