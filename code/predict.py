97c6cc33e15265f025eee544533f5cade7e10c05
nktshch <nktshch@yandex.ru>
2025-02-05 10:43:15 +0300

    added basis for analyzing order

--- code/predict.py
+++ code/predict.py
@@ -16,6 +16,7 @@ from pathlib import Path
 import fasttext
 import pyconll
 from tqdm import tqdm
+import re
 
 
 device = 'cuda' if cuda.is_available() else 'cpu'
@@ -54,8 +55,65 @@ def predictions_to_grammemes(vocabulary, predictions):
     return tags
 
 
+def predict_sentence(sentence, output):
+    conf, vocab, model = load_model_vocab("./trained_models/direct/Russian/seed_0/model.pt",
+                                          "./trained_models/direct/Russian/vocab.pickle")
+    word_list = re.findall(r"\b\w+(?:-\w+)*\b|[^\w\s]", sentence)
+    print(word_list)
+
+    train_words = set(vocab.vocab["word-index"])
+    words = set()
+    for word in word_list:
+        if word.isdigit():
+            words.add(conf['NUM'])
+        else:
+            words.add(word.lower())
+    uniq_words = words - train_words
+
+    print("Loading fastText...")
+    ft = fasttext.load_model("./filled_conllu/wiki.ru.bin")
+    oov_pretrained_vocab = {}
+    for word in ft.words:
+        if word.lower() in uniq_words:
+            oov_pretrained_vocab[word.lower()] = ft[word]
+
+    words, labels = vocab.sentence_to_indices(word_list, None, training=False)
+    words, chars, _, __ = collate_batch([((words, labels), word_list)])
+    words = words.to(device)
+    chars = chars.to(device)
+
+    unk_id = vocab.vocab["word-index"][conf["UNK"]]
+    oov = None
+    fasttext_embeddings = []
+    mask_embeddings = torch.zeros(words.shape, dtype=bool, device=device)
+    for i, word_index in enumerate(words):
+        if word_index.item() == unk_id:
+            if word_list[i].lower() in oov_pretrained_vocab.keys():
+                fasttext_embeddings += [oov_pretrained_vocab[word_list[i].lower()]]
+                mask_embeddings[i] = True
+
+    if torch.any(mask_embeddings):
+        fasttext_embeddings = torch.tensor(np.array(fasttext_embeddings)).to(device)
+        assert mask_embeddings.sum().item() == fasttext_embeddings.shape[0]
+        oov = (fasttext_embeddings, mask_embeddings)
+
+    with torch.no_grad():
+        predictions, probabilities = model(words, chars, None, oov=oov)
+    grammemes = predictions_to_grammemes(vocab.vocab, predictions.permute(1, 0))
+
+    with open(output, "w+", encoding='utf-8') as txt:
+        for label, word in zip(grammemes, word_list):
+            s = []
+            for g in label:
+                if g == "$EOS$":
+                    break
+                s.append(g)
+            txt.write(f"{word} - {s}\n")
+
+
 def fill_conllu(conf, vocab, model, uniq_words, conll_input, conll_output):
     conll = pyconll.load_from_file(conll_input)
+    txt_output = conll_output[:-6] + "txt"
 
     print(f"{len(uniq_words)} oov words")
     print("Loading fastText...")
@@ -67,66 +125,119 @@ def fill_conllu(conf, vocab, model, uniq_words, conll_input, conll_output):
 
     print("Filling file...")
     progress_bar = tqdm(conll, total=len(conll), colour='#bbbbbb')
-    for number, sentence in enumerate(progress_bar):
-        word_list = []
-        for token in sentence:
-            if '.' not in token.id and '-' not in token.id:
-                word_list += [token.form]
+    with open(txt_output, "w+", encoding='utf-8') as txt:
+        for number, sentence in enumerate(progress_bar):
+            word_list = []
+            for token in sentence:
+                if '.' not in token.id and '-' not in token.id:
+                    word_list += [token.form]
 
-        words, labels = vocab.sentence_to_indices(word_list, None, training=False)
-        words, chars, _, __ = collate_batch([((words, labels), word_list)])
-        words = words.to(device)
-        chars = chars.to(device)
+            words, labels = vocab.sentence_to_indices(word_list, None, training=False)
+            words, chars, _, __ = collate_batch([((words, labels), word_list)])
+            words = words.to(device)
+            chars = chars.to(device)
 
-        unk_id = vocab.vocab["word-index"][conf["UNK"]]
-        oov = None
-        fasttext_embeddings = []
-        mask_embeddings = torch.zeros(words.shape, dtype=bool, device=device)
-        for i, word_index in enumerate(words):
-            if word_index.item() == unk_id:
-                if word_list[i].lower() in oov_pretrained_vocab.keys():
-                    fasttext_embeddings += [oov_pretrained_vocab[word_list[i].lower()]]
-                    mask_embeddings[i] = True
+            unk_id = vocab.vocab["word-index"][conf["UNK"]]
+            oov = None
+            fasttext_embeddings = []
+            mask_embeddings = torch.zeros(words.shape, dtype=bool, device=device)
+            for i, word_index in enumerate(words):
+                if word_index.item() == unk_id:
+                    if word_list[i].lower() in oov_pretrained_vocab.keys():
+                        fasttext_embeddings += [oov_pretrained_vocab[word_list[i].lower()]]
+                        mask_embeddings[i] = True
 
-        if torch.any(mask_embeddings):
-            fasttext_embeddings = torch.tensor(np.array(fasttext_embeddings)).to(device)
-            assert mask_embeddings.sum().item() == fasttext_embeddings.shape[0]
-            oov = (fasttext_embeddings, mask_embeddings)
+            if torch.any(mask_embeddings):
+                fasttext_embeddings = torch.tensor(np.array(fasttext_embeddings)).to(device)
+                assert mask_embeddings.sum().item() == fasttext_embeddings.shape[0]
+                oov = (fasttext_embeddings, mask_embeddings)
 
-        with torch.no_grad():
-            predictions, probabilities = model(words, chars, None, oov=oov)
-        grammemes = predictions_to_grammemes(vocab.vocab, predictions.permute(1, 0))
+            with torch.no_grad():
+                predictions, probabilities = model(words, chars, None, oov=oov)
+            grammemes = predictions_to_grammemes(vocab.vocab, predictions.permute(1, 0))
 
-        for label, token in zip(grammemes, sentence):
-            other_grammemes = [g for g in label if "POS" not in g]
-            pos_grammeme = next(iter([g for g in label if "POS" in g]), "POS=Did_not_predict")
-            token.upos = pos_grammeme[4:]
-            feats = defaultdict(list)
-            for grammeme in other_grammemes:
-                if grammeme == conf["EOS"]:
-                    break
-                key, feat = grammeme.split("=")
-                feats[key] += [feat]
-            token.feats = feats
+            for label, token in zip(grammemes, sentence):
+                if '.' not in token.id and '-' not in token.id:
+                    txt.write(f"{token.form} -")
+                    for g in label:
+                        if g == conf["EOS"]:
+                            break
+                        txt.write(f" {g}")
+                    txt.write("\n")
+                    # other_grammemes = [g for g in label if "POS" not in g]
+                    # pos_grammeme = next(iter([g for g in label if "POS" in g]), "POS=Did_not_predict")
+                    # token.upos = pos_grammeme[4:]
+                    # feats = defaultdict(list)
+                    # for grammeme in other_grammemes:
+                    #     if grammeme == conf["EOS"]:
+                    #         break
+                    #     key, feat = grammeme.split("=")
+                    #     feats[key] += [feat]
+                    # token.feats = feats
 
-    with open(conll_output, 'w+', encoding='utf-8') as f:
-        conll.write(f)
+    # with open(conll_output, 'w+', encoding='utf-8') as f:
+    #     conll.write(f)
+
+
+def analyze_order(txt_input, seed):
+    with open(txt_input, "r", encoding='utf-8') as txt:
+        lines = txt.readlines()
+        # print(len(lines))
+
+    data = []
+    categories = set()
+    for line in lines:
+        features = {}
+        word, grammemes = line.split(" - ")
+        features["Form"] = word
+        for i, g in enumerate(grammemes.split()):
+            cat, val = g.split("=")
+            features[cat + "_value"] = val
+            features[cat + "_position"] = i
+            categories.add(cat)
+        data.append(features)
+
+    categories = sorted(list(categories), key=len)
+    columns = ["Form"] + [cat + "_value" for cat in categories] + [cat + "_position" for cat in categories]
+
+    df = pd.DataFrame(data=data, columns=columns)
+    print(f"TOTAL WORDS: {len(df)}")
+    # df.to_excel(f"statistics_oa_{seed}.xlsx", na_rep="none")
+    # print(df[df['POS_value'].notnull()])
+
+    dataframes = [(df, "full")]
+
+    df_pos_case = df[df[['POS_value', 'Case_value']].notnull().all(1)]
+    dataframes.append((df_pos_case, "pos-case"))
+    # print(df_pos_case[['POS_value', 'Case_value', 'POS_position', 'Case_position']])
+
+    df_pos_animacy = df[df[['POS_value', 'Animacy_value']].notnull().all(1)]
+    dataframes.append((df_pos_animacy, "pos-animacy"))
+    # print(df_pos_animacy[['POS_value', 'Animacy_value', 'POS_position', 'Animacy_position']])
+
+    df_tense_animacy = df[df[['Tense_value', 'Animacy_value']].notnull().all(1)]
+    dataframes.append((df_tense_animacy, "tense-animacy"))
+    # print(df_tense_animacy[['Form', 'POS_value', 'Tense_value', 'Animacy_value']])
+
+    df_tense_case = df[df[['Tense_value', 'Case_value']].notnull().all(1)]
+    dataframes.append((df_tense_case, "tense-case"))
+    # print(df_tense_case[['Form', 'POS_value', 'Tense_value', 'Case_value']])
+
+    # for dataframe, name in dataframes:
+    #     for cat in categories:
+    #         print(f"{cat}_mean {name}: {dataframe[cat + '_position'].mean()}")
 
 
 def get_uniq_words(conf, vocab, conll):
     train_words = set(vocab.vocab["word-index"])
     test_words = set()
-    test_with_digits = set()
-    digits = set()
     for sentence in conll:
         for word in sentence:
             if '.' not in word.id and '-' not in word.id:
                 if word.form.isdigit():
                     test_words.add(conf['NUM'])
-                    digits.add(word.form)
                 else:
                     test_words.add(word.form.lower())
-                test_with_digits.add(word.form.lower())
     unseen_words = test_words - train_words
     return unseen_words
 
@@ -252,7 +363,7 @@ def analyze_results():
                 file.write(f"{key}: {m_:.5} ± {s_:.5}\n")
 
 
-def evaluate_models(language="Russian", language_short="ru", dataset="dev"):
+def evaluate_models(language="Russian", language_short="ru", dataset="train"):
     """Uses trained models to fill conllu files, then evaluates metrics and writes results to .txt files.
 
     Args:
@@ -268,7 +379,7 @@ def evaluate_models(language="Russian", language_short="ru", dataset="dev"):
     # temp = pathlib.PosixPath
     # pathlib.PosixPath = pathlib.WindowsPath
     for seed_number in range(5):
-        model_paths = [str(x) for x in p.glob(f"*/{language}/seed_{seed_number}/model.pt")]
+        model_paths = [str(x) for x in p.glob(f"*order_agnostic/{language}/seed_{seed_number}/model.pt")]
         vocab_paths = [str(x) for x in p.glob(f"*/{language}/vocab.pickle")]
 
         for model_path, vocab_path in zip(model_paths, vocab_paths):
@@ -286,13 +397,17 @@ def evaluate_models(language="Russian", language_short="ru", dataset="dev"):
 
             # analyze_df(df)
 
-            with open(f"./filled_conllu/results-{language_short}-{dataset}-{model_name}-{seed_number}.txt", "w+") as file:
-                file.write(f"{language}, {dataset}, {model_name}\n")
-                results = calculate_accuracy_df(df)
-                for key in results:
-                    file.write(f"{key}: {results[key]:.5}\n")
+            # with open(f"./filled_conllu/results-{language_short}-{dataset}-{model_name}-{seed_number}.txt", "w+") as file:
+            #     file.write(f"{language}, {dataset}, {model_name}\n")
+            #     results = calculate_accuracy_df(df)
+            #     for key in results:
+            #         file.write(f"{key}: {results[key]:.5}\n")
 
 
 if __name__ == "__main__":
-    evaluate_models()
-    analyze_results()
+    # evaluate_models(dataset="dev")
+    # analyze_results()
+    # predict_sentence("Каждый охотник желает знать, где сидит фазан.", "prediction.txt")
+    for seed in range(1):
+        print(f"====================\norder_agnostic run {seed}")
+        analyze_order(f"filled_conllu/Russian/ru-ud-train_order_agnostic_{seed}.txt", seed=seed)
